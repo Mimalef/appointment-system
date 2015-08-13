346d16276142b869a4735ec592ef49900c07a2d5
Mojtaba <mojtabaahmadi40@gmail.com>
2015-08-13 20:17:23 +0430

    fix indent

--- src/admin_panel.php
+++ src/admin_panel.php
@@ -1,6 +1,6 @@
 <?php include "_header.php"; ?>
-<a class="button" href="admin_list_doctors.php">لیست پزشکان</a>
-<a class="button" href="admin_list_patients.php">لیست بیماران</a>
-<a class="button" href="admin_signup_doctor.php">ثبت پزشک</a>
-<a class="button" href="admin_edit.php">ویرایش خود</a>
+    <a class="button" href="admin_list_doctors.php">لیست پزشکان</a>
+    <a class="button" href="admin_list_patients.php">لیست بیماران</a>
+    <a class="button" href="admin_signup_doctor.php">ثبت پزشک</a>
+    <a class="button" href="admin_edit.php">ویرایش خود</a>
 <?php include "_footer.php"; ?>
