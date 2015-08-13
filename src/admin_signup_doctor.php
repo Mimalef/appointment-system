77509fb19ba74e518b2908187fe4164760d6b6f3
Mojtaba <mojtabaahmadi40@gmail.com>
2015-08-13 22:52:37 +0430

    update "admin_signup_doctor.php" for new fields

--- src/admin_signup_doctor.php
+++ src/admin_signup_doctor.php
@@ -1,15 +1,33 @@
 <?php
-include "_conn.php";
-include "_header.php";
 include "_admin_permission.php";
+include "_header.php";
+include "_conn.php";
 
 if(isset($_GET['submit']))
 {
     $name = $_GET['name'];
     $spec = $_GET['spec'];
     $pass = $_GET['pass'];
+    $user = $_GET['user'];
+    $tele = $_GET['tele'];
+    $addr = $_GET['addr'];
 
-    $sql = "INSERT INTO doctors VALUES(NULL, '$name', '$spec', '$pass')";
+    $sql = "
+        INSERT INTO doctors(
+            name,
+            specialist,
+            password,
+            username,
+            telephone,
+            address)
+        VALUES (
+            '$name',
+            '$spec',
+            '$pass',
+            '$user',
+            $tele,
+            '$addr')";
+
     $res = $db->query($sql);
 
     if($res)
@@ -24,10 +42,13 @@ if(isset($_GET['submit']))
 ?>
 
 <form>
-    <input name="name" type="text" placeholder="نام" >
-    <input name="spec" type="text" placeholder="تخصص" >
-    <input name="pass" type="text" placeholder="گذرواژه" >
-    <input name="submit" type="submit"  value="ثبت" >
+    <input name="name" type="text" placeholder="نام">
+    <input name="spec" type="text" placeholder="تخصص">
+    <input name="pass" type="text" placeholder="گذرواژه">
+    <input name="user" type="text" placeholder="نام کاربری">
+    <input name="tele" type="text" placeholder="تلفن">
+    <input name="addr" type="text" placeholder="آدرس">
+    <input name="submit" type="submit"  value="ثبت">
 </form>
 
 <?php include "_footer.php"; ?>
