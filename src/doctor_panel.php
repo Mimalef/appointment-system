5d1e4a577cc63804cac1b85082db17d9df2652e3
Mojtaba <mojtabaahmadi40@gmail.com>
2015-08-14 15:42:30 +0430

    fix panels syntax

--- src/doctor_panel.php
+++ src/doctor_panel.php
@@ -2,7 +2,8 @@
     include "_doctor_permission.php";
     include "_header.php";
 ?>
-    <a class="button" href="doctor_schedule.php">برنامه روزانه</a>
-    <a class="button" href="doctor_edit.php">ویرایش اطلاعات</a>
-<?php include "_footer.php"; ?>
 
+<a class="button" href="doctor_schedule.php">برنامه روزانه</a>
+<a class="button" href="doctor_edit.php">ویرایش اطلاعات</a>
+
+<?php include "_footer.php"; ?>
