c60b78977c7ab60762eb2a8ae064bf4dd03488b8
Mojtaba <mojtabaahmadi40@gmail.com>
2015-08-14 13:49:18 +0430

    some newline

--- src/admin_panel.php
+++ src/admin_panel.php
@@ -2,8 +2,10 @@
     include "_admin_permission.php";
     include "_header.php";
 ?>
+
     <a class="button" href="admin_list_doctors.php">لیست پزشکان</a>
     <a class="button" href="admin_list_patients.php">لیست بیماران</a>
     <a class="button" href="admin_signup_doctor.php">ثبت پزشک</a>
     <a class="button" href="admin_edit.php">ویرایش خود</a>
+
 <?php include "_footer.php"; ?>
