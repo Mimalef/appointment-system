59dad617a3f52eab5b3ff55fb7c093caca9d1927
omid23 <omidbeigi23@gmail.com>
2015-08-14 07:06:21 -0700

    :)

--- src/admin_delete_doctor.php
+++ src/admin_delete_doctor.php
@@ -1,1 +1,11 @@
-
+<?php
+    include "_conn.php";
+    include "_header.php";
+    include "_admin_permission.php";
+    if(isset($_GET['id']))
+{
+    $doctordelete = $_GET['id'];
+    $sql = "DELETE From doctors Where id = $doctordelete";
+    $res = $db->query($sql);
+    header('location: admin_list_doctors.php');}
+?>
