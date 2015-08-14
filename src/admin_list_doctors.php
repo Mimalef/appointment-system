d2a2b48dcf8ac44d27c99b69cceca550dc20ffaf
omid23 <omidbeigi23@gmail.com>
2015-08-13 22:42:03 -0700

    No more errors!

--- src/admin_list_doctors.php
+++ src/admin_list_doctors.php
@@ -1,20 +1,13 @@
 <?php
-session_start();
+    include "_conn.php";
+    include "_header.php";
+    include "_admin_permission.php";
+    $sql = "SELECT * FROM doctors";
+    $res = $db->query($sql);
+    $res = $res->fetchAll();
 
-include "conn.php";
-include "_header.php";
-include "_admin_permission";
-if(!isset($_SESSION['doctor_id']))
-{
-    header('location: index.php');
-}
-
-$doctor_id = $_SESSION['doctor_id'];
-
-$sql = "SELECT * FROM doctors WHERE doctor = $doctor_id";
-$res = $db->query($sql);
-$res = $res->fetchAll();
 ?>
+<form>
 <table id="doctor">
     <tr>
         <th>نام</th>
@@ -22,17 +15,18 @@ $res = $res->fetchAll();
         <th>تلفن</th>
         <th>تخصص</th>
         <th>ادرس</th>
-
     </tr>
+
 <?php foreach($res as $row) { ?>
     <tr>
         <td><?php echo $row['name'] ?></td>
-        <td><?php echo $row['pass'] ?></td>
-        <td><?php echo $row['tel'] ?></td>
-        <td><?php echo $row['spechialist'] ?></td>
+        <td><?php echo $row['password'] ?></td>
+        <td><?php echo $row['telephone'] ?></td>
+        <td><?php echo $row['specialist'] ?></td>
         <td><?php echo $row['address'] ?></td>
 
     </tr>
-<?php } ?>
+<?php }  ?>
 </table>
+</form>
 <?php include "_footer.php"; ?>
