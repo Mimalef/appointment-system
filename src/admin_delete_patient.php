4fd4b37c5774cb286b65a667abef2e9cf67bd216
Mojtaba <mojtabaahmadi40@gmail.com>
2015-08-14 19:47:02 +0430

    new update

--- src/admin_delete_patient.php
+++ src/admin_delete_patient.php
@@ -1,1 +1,29 @@
+<?php
+include "_admin_permission.php";
+include "_header.php";
+include "_conn.php";
 
+$patient_id = $_GET['id'];
+
+if(isset($_GET['submit']))
+{
+    $sql = "
+        DELETE FROM
+            patients
+        WHERE
+            id=$patient_id";
+
+    $res = $db->query($sql);
+
+    if($res)
+    {
+        header("location: admin_list_patients.php");
+    }
+    else
+    {
+        echo "<p class='erro>خطا</p>";
+    }
+}
+
+include "_footer.php";
+?>
