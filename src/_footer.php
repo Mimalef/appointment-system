858a025d38094f088b0a1acee69fdb6e6423ad91
Mojtaba <mojtabaahmadi40@gmail.com>
2015-08-21 02:29:49 +0430

    Update name of creator

--- src/_footer.php
+++ src/_footer.php
@@ -1,6 +1,6 @@
         </div>
         <div id="s"></div>
-        <div id="footer"><p>Copyright &#169; 2015 Rezayi</p></div>
+        <div id="footer"><p>Copyright &#169; 2015 Rezayi & Sadeghi</p></div>
         <div id="s"></div>
     </div>
 </body>
